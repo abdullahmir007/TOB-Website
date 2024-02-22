0c03ac317d250a1f2dd9e58dcfd072c409dd2452
abdullahmir007 <abdullahmir073@gmail.com>
2024-02-22 21:28:35 +0500

    Add changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/Cloud.jpg";
+import image from "../images/Cloud.png";
 
 const imageAltText = "Cloud Computing related background";
 
