54ebc8e511a8be51c2afede339f457f95c342501
abdullahmir007 <abdullahmir073@gmail.com>
2024-02-22 21:16:30 +0500

    Add Changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/Cloud.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Cloud Computing related background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a CLoud Enthusiast student studying at Bahria University. I enjoy working on CLoud resources like Server-administration,CLoud Enhancement.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "Cloud Computing",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "User Testing",
+  "Unit testing",
+  "Mobile-user-interfaces",
+  "Graphic developement",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "THis is my Portfolio website and I am in Bahria University Islamabad working as a Student Ambassador For Microsoft.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/Cloud 2.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Cloud Upbringing ";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: " Personal website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This is my personal Website.",
+    url: "https://abdullahmir007.github.io/website/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Linkdin Profile",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "This is my linkdin Profile where I connect with like minded people.",
+    url: "https://www.linkedin.com/in/mir-abdullah-rizwan/",
   },
   {
-    title: "My Resume Site",
+    title: "Hacktoberfest Webpage",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "This is a Basic HTML Hcktoberfest page.",
+    url: "https://abdullahmir007.github.io/hacktoberfest-html/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Github",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "My Official Github Account.",
+    url: "https://github.com/abdullahmir007",
   },
 ];
 
